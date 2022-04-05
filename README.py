d4d61a65c3e3624a1763c85a951c2e95828b0425
razamuhamadfazrin11 <103030733+razamuhamadfazrin11@users.noreply.github.com>
2022-04-05 15:55:32 +0700

    Create README.py

---
+++ README.py
@@ -0,0 +1,16 @@
+- 👋 Hi, I’m @razamuhamadfazrin11
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+razamuhamadfazrin11/razamuhamadfazrin11 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
+Pilih huruf besar atau kecil dalam kurung: x
+Ketik huruf yang benar: M
+Ketik huruf yang benar: k
+Ketik kata: amal
+amal kata asimetris
+amal kata tidak bolak-balik
